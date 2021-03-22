467bd0574fc5183ada53b2ff497157afd04c73e8
Jorge Gutiérrez <34510304+Yorchly@users.noreply.github.com>
2021-03-22 19:19:08 +0100

    Callable type hint in constructor

--- python_tests/menu/menu_option.py
+++ python_tests/menu/menu_option.py
@@ -1,9 +1,12 @@
+from typing import Callable
+
+
 class MenuOption:
     option_name = ""
     option_func = None
     option_func_kwargs = None
 
-    def __init__(self, option_name: str, option_func: any, option_func_kwargs: dict):
+    def __init__(self, option_name: str, option_func: Callable, option_func_kwargs: dict):
         self.option_name = option_name
         self.option_func = option_func
         self.option_func_kwargs = option_func_kwargs

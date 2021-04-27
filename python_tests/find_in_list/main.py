b010b84594070da387fbb7ff35279889515a5ee7
Jorge Gutiérrez <34510304+Yorchly@users.noreply.github.com>
2021-04-27 20:52:14 +0200

    Added more tests

--- python_tests/find_in_list/main.py
+++ python_tests/find_in_list/main.py
@@ -17,19 +17,27 @@ def search_with_filter(elements: list, searched_element: int) -> int:
     return list(filter(lambda element: element == searched_element, elements))[0]
 
 
+def search_with_comprehension(elements: list, searched_element: int) -> int:
+    return [element for element in elements if element == searched_element][0]
+
+
+def search_with_next(elements: list, searched_element: int) -> int:
+    return next(element for element in elements if element == searched_element)
+
+
 def start_searching():
     try:
         element_number = int(input("Introduce number of elements in list: "))
     except ValueError:
         logger.error("Error with explicit conversion of number of elements, using default value (1000000)")
-        element_number=1000000
+        element_number = 1000000
 
-    elements = [number for number in range(1, element_number)]
+    elements = list(range(0, element_number))
+    functions = (
+        search_with_loop, search_with_filter, search_with_comprehension, search_with_next
+    )
 
-    exec_time = execution_time(search_with_loop, elements=elements, searched_element=element_number - 1)
-    if exec_time:
-        print(f"Time searching element {exec_time[1]} with loops: {exec_time[0]}")
-
-    exec_time = execution_time(search_with_filter, elements=elements, searched_element=element_number - 1)
-    if exec_time:
-        print(f"Time searching element {exec_time[1]} with filters: {exec_time[0]}")
+    for function in functions:
+        exec_time = execution_time(function, elements=elements, searched_element=element_number - 1)
+        if exec_time:
+            print(f"Time searching element {exec_time[1]} with '{function.__name__}': {exec_time[0]}")
